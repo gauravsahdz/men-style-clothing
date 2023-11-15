6cf5b1c64c708eec95642890f9c28b863c248777
gauravsahdz <sahgaurav33@gmail.com>
2023-11-15 23:54:43 +0545

    signin page issue fixed

--- pages/SigninPage/index.tsx
+++ pages/SigninPage/index.tsx
@@ -33,7 +33,7 @@ const Signin = () => {
     if (session?.user) {
       router.push("/");
     }
-  }, [session]);
+  }, []);
 
   return (
     <div className="min-h-screen flex items-center justify-center flex-col bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
