5a5c27ca9c813ae07bbd049c9629d780cc9cd2e7
gauravsahdz <sahgaurav33@gmail.com>
2023-12-03 21:34:24 +0545

    meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Nav from "@/components/Nav";
 import Footer from "@/components/Footer";
 import AuthProvider from "@/Context/AuthProvider";
 import LoaderProvider from "@/Context/LoaderProvider";
+import Head from "next/head";
 
 export default function RootLayout({
   children,
@@ -14,6 +15,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <title>Home</title>
+        <meta
+          name="description"
+          content="
+          Modern T-shirt and Hoodies for your everyday style.
+        "
+        />
+        <meta name="keywords" content="men-style" />
+        <meta name="author" content="Gaurav Sah" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <body>
         <AuthProvider session={null}>
           <LoaderProvider>
